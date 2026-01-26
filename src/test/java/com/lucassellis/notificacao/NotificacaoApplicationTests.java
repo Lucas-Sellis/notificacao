930e6a1b083041b4124a9dca2de00516f0d2f9ec
Lucas <lucas.sellis@gmail.com>
2026-01-26 08:21:12 -0300

    commit inicial

---
+++ src/test/java/com/lucassellis/notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucassellis.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
